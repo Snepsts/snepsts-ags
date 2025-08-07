ab9cfdc0784400d2e4f25c55816848cd41779991
Michael Ranciglio <rancigliom@gmail.com>
2025-08-06 21:31:39 -0500

    Update ESLint and use Prettier for formatting

--- eslint.config.js
+++ eslint.config.js
@@ -1,37 +1,10 @@
 // @ts-check
 import eslint from '@eslint/js'
 import tslint from 'typescript-eslint'
+import prettierConfig from 'eslint-config-prettier';
 
 export default tslint.config(
 	eslint.configs.recommended,
-	...tslint.configs.recommended,
-	{
-		rules: {
-	// 		'@typescript-eslint/quotes': ['warn', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
-			'quotes': ['warn', 'single', { avoidEscape: true, allowTemplateLiterals: true }]
-
-		}
-	}
+	tslint.configs.recommended,
+	prettierConfig
 )
-
-// export default antfu(
-// 	{
-// 		typescript: {
-// 			tsconfigPath: 'tsconfig.json',
-// 		},
-// 		// antfu overrides
-// 		rules: {
-// 			'no-console': 'off',
-// 			// 'indent': ['error', 2],
-// 			'quotes': ['warn', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
-// 			// 'no-undef': 'off',
-// 			'brace-style': '1tbs',
-// 			// 'unused-imports/no-unused-vars': 'off',
-// 			// 'no-useless-return': 'off',
-// 			'style/no-tabs': 'off',
-// 			'style/indent': ['error', 'tab'],
-// 			'jsonc/indent': ['error', 'tab'],
-// 			'antfu/consistent-list-newline': 'off',
-// 		},
-// 	}
-// )
